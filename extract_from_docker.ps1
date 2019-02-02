357550d3d719b5bdc9814e30ac02ad171d9b6590
Aki Ariga <chezou+github@gmail.com>
2019-02-03 00:51:34 +0900

    Refactor scripts

--- extract_from_docker.ps1
+++ extract_from_docker.ps1
@@ -1,25 +1,34 @@
 Param(
-    [String]$VERSION = "3.4.1.p0.1",
+    [String]$PARCEL_VERSION = "3.4.1.p0.1",
     [String]$DISTRIBUTION = "centos",
     [String]$TARGET_OS = "centos7",
     [String]$OS_VERSION = "el7",
     [String]$R_VERSION = "3.5.1",
-    [String]$ARROW_VERSION = "0.12.0"
+    [String]$ARROW_VERSION = "0.12.0",
+    [String]$CONDA_URI = "https://repo.continuum.io/miniconda/Miniconda2-latest-Linux-x86_64.sh",
+    [String]$PARCEL_NAME= "CONDAR",
+    [String]$PARCEL_DIR = "/opt/cloudera/parcels"
 )
 
 $TIME= [int][double]::Parse((Get-Date -UFormat "%s"))
 $CONTAINER="parcel-build-${TIME}"
 
-Write-Output "Create parcel: CONDAR-${VERSION}-${OS_VERSION}.parcel"
+$IMAGE_NAME=${PARCEL_NAME}.ToLower()
 
-docker build --no-cache --build-arg VERSION="${VERSION}" --build-arg OS_VERSION="${OS_VERSION}" --build-arg TARGET_OS="${TARGET_OS}" --build-arg R_VERSION="${R_VERSION}" --build-arg ARROW_VERSION="${ARROW_VERSION}" -t chezou/condar:"${TARGET_OS}" -f "${DISTRIBUTION}.Dockerfile" .
-docker run -d --name "${CONTAINER}" -t chezou/condar:"${TARGET_OS}" /bin/bash
-docker cp -L "${CONTAINER}:/data/CONDAR-${VERSION}-${OS_VERSION}.parcel" ./target/
+Write-Output "Create parcel: ${PARCEL_NAME}-${PARCEL_VERSION}-${OS_VERSION}.parcel"
+
+docker build --no-cache --build-arg PARCEL_NAME="${PARCEL_NAME}" --build-arg PARCEL_VERSION="${PARCEL_VERSION}" `
+        --build-arg PARCEL_DIR="${PARCEL_DIR}" --build-arg CONDA_URI="${CONDA_URI}" `
+        --build-arg OS_VERSION="${OS_VERSION}" --build-arg TARGET_OS="${TARGET_OS}" `
+        --build-arg R_VERSION="${R_VERSION}" --build-arg ARROW_VERSION="${ARROW_VERSION}" `
+        -t "${IMAGE_NAME}:${TARGET_OS}" -f "${DISTRIBUTION}.Dockerfile" .
+docker run -d --name "${CONTAINER}" -t "${IMAGE_NAME}:${TARGET_OS}" /bin/bash
+docker cp -L "${CONTAINER}:${PARCEL_DIR}/${PARCEL_NAME}-${PARCEL_VERSION}-${OS_VERSION}.parcel" ./target/
 docker kill $CONTAINER
 docker rm $CONTAINER
 
 # validation
 Write-Output "Validation"
-java -jar lib/validator.jar -f "target/CONDAR-${VERSION}-${OS_VERSION}.parcel"
+java -jar lib/validator.jar -f "target/${PARCEL_NAME}-${PARCEL_VERSION}-${OS_VERSION}.parcel"
 
-Write-Output "Success to create CONDAR-${VERSION}-${OS_VERSION}.parcel"
+Write-Output "Success to create ${PARCEL_NAME}-${PARCEL_VERSION}-${OS_VERSION}.parcel"
--- source/meta/R_env.sh
+++ source/meta/R_env.sh
@@ -1,27 +1,25 @@
 #!/bin/bash
 
-CONDAR_DIRNAME=${PARCEL_DIRNAME:-"CONDAR-3.4.1"}
-
 if [ -n "${R_HOME}" ]; then
-  export R_HOME="${R_HOME}:$PARCELS_ROOT/$CONDAR_DIRNAME/lib/conda-R/lib"
+  export R_HOME="${R_HOME}:$PARCELS_ROOT/__PARCEL_NAME__-__PARCEL_VERSION__/lib/r/lib"
 else
-  export R_HOME="$PARCELS_ROOT/$CONDAR_DIRNAME/lib/conda-R/lib"
+  export R_HOME="$PARCELS_ROOT/__PARCEL_NAME__-__PARCEL_VERSION__/lib/r/lib"
 fi
 
 if [ -n "${RHOME}" ]; then
-  export RHOME="${RHOME}:$PARCELS_ROOT/$CONDAR_DIRNAME/lib/conda-R"
+  export RHOME="${RHOME}:$PARCELS_ROOT/__PARCEL_NAME__-__PARCEL_VERSION__/lib/r"
 else
-  export RHOME="$PARCELS_ROOT/$CONDAR_DIRNAME/lib/conda-R"
+  export RHOME="$PARCELS_ROOT/__PARCEL_NAME__-__PARCEL_VERSION__/lib/r"
 fi
 
 if [ -n "${R_SHARE_DIR}" ]; then
-  export R_SHARE_DIR="${R_SHARE_DIR}:$PARCELS_ROOT/$CONDAR_DIRNAME/lib/conda-R/lib/R/share"
+  export R_SHARE_DIR="${R_SHARE_DIR}:$PARCELS_ROOT/__PARCEL_NAME__-__PARCEL_VERSION__/lib/r/lib/R/share"
 else
-  export R_SHARE_DIR="$PARCELS_ROOT/$CONDAR_DIRNAME/lib/conda-R/lib/R/share"
+  export R_SHARE_DIR="$PARCELS_ROOT/__PARCEL_NAME__-__PARCEL_VERSION__/lib/r/lib/R/share"
 fi
 
 if [ -n "${R_INCLUDE_DIR}" ]; then
-  export R_INCLUDE_DIR="${R_INCLUDE_DIR}:$PARCELS_ROOT/$CONDAR_DIRNAME/lib/conda-R/lib/R/include"
+  export R_INCLUDE_DIR="${R_INCLUDE_DIR}:$PARCELS_ROOT/__PARCEL_NAME__-__PARCEL_VERSION__/lib/r/lib/R/include"
 else
-  export R_INCLUDE_DIR="$PARCELS_ROOT/$CONDAR_DIRNAME/lib/conda-R/lib/R/include"
+  export R_INCLUDE_DIR="$PARCELS_ROOT/__PARCEL_NAME__-__PARCEL_VERSION__/lib/r/lib/R/include"
 fi
